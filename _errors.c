ea63e1e6effa66e73c8d708da1f6d6a637015371
ShanyceMudai <jepchumbashanyce@gmail.com>
2023-12-06 13:42:41 -0800

    exit built-in, that exits the shell

---
+++ _errors.c
@@ -0,0 +1,94 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints an input string
+ *
+ * @str: string
+ *
+ * Return: no return
+ *
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - writes the character to
+ * STDERR
+ *
+ * @c: character
+ *
+ * Return: 1 on success
+ *
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes the character c to given fd
+ *
+ * @c: character
+ *
+ * @fd: filedescriptor
+ *
+ * Return: 1 on success
+ *
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string
+ *
+ * @str: string
+ *
+ * @fd: filedescriptor
+ *
+ * Return: the number of chars
+ *
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
